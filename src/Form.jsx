cf6f1cab7d000e6c592b0097eed9f2f5510c9a38
gkn46 <gokhan.cingoz46@gmail.com>
2024-05-08 23:08:23 +0300

    tailwin css eklendi, ikonlar için lucid eklendi, yeni kayıt sayfası pop-up şeklinde güncellendi, kullanıcı listesi görünümünde iyileştirmeler yapıldı.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-
-function Form() {
+import { useState,useRef } from "react";
+function Form({onClose}) {
   const [formData, setFormData] = useState({
     name: '',
     surname: '',
@@ -15,8 +14,8 @@ function Form() {
       [name]: type === 'checkbox' ? checked : value
     }));
   };
-
   const handleSave = async () => {
+
     try {
       const response = await fetch('http://employee-api.runasp.net/api/Employee', {
         method: 'POST',
@@ -27,10 +26,11 @@ function Form() {
       });
 
       if (response.ok) {
-        console.log('Employee successfully saved.');
+        window.alert("Kayıt gerçekleşti");
         // İşlem başarılıysa gerekli adımları buraya ekleyebilirsiniz.
       } else {
         console.error('Failed to save employee.');
+        window.alert("Kayıt gerçekleşmedi");
         // İşlem başarısızsa kullanıcıya bir hata mesajı gösterebilirsiniz.
       }
     } catch (error) {
@@ -39,33 +39,33 @@ function Form() {
   };
 
   return (
-    <>
+    <div>
       <form>
         <div className="input-group mb-3">
         <span className="input-group-text" id="inputGroup-sizing-default">İsim:</span>
-        <input type="text" className="form-control" name="name" value={formData.name} onChange={handleChange}/>
+        <input type="text" className="form-control" name="name" required value={formData.name} onChange={handleChange}/>
         </div>
 
         <div className="input-group mb-3">
         <span className="input-group-text" id="inputGroup-sizing-default">Soy İsim:</span>
-        <input type="text" className="form-control"  name="surname" value={formData.surname} onChange={handleChange}/>
+        <input type="text" className="form-control"  name="surname" required value={formData.surname} onChange={handleChange}/>
         </div>
 
         <div className="input-group mb-3">
         <span className="input-group-text" id="inputGroup-sizing-default">Departman:</span>
-        <input type="text" className="form-control" name="department" value={formData.department} onChange={handleChange} />
+        <input type="text" className="form-control" name="department" required value={formData.department} onChange={handleChange} />
         </div>
 
         <div className="input-group mb-3">
         <div className="input-group-text">
-          <input type="checkbox" className="form-check-input" name="isReferance" checked={formData.isReferance} onChange={handleChange}/>
+          <input type="checkbox" className="form-check-input" name="isReferance" required checked={formData.isReferance} onChange={handleChange}/>
         </div>
         <label className="form-control">Referansı var mı?</label>
         </div>
 
-        <button type="button" className="btn btn-secondary" onClick={handleSave}>Kaydet</button>
+        <button type="button" className="btn btn-secondary w-20 h-30" onClick={handleSave}>Kaydet</button>
       </form>
-    </>
+    </div>
   );
 }
 
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Employes from "./Employes.jsx"
 import AddEmploye from './AddEmploye'
 
 function MainPage() {
+  const [showModal, setShowModal]=useState(false)
   return (
-    <div>
-      <AddEmploye />
+    <div className='h-screen flex flex-col items-center gap-6 bg-white text-black'>
+            <h1 className='text-5xl font-bold mt-4'>Personel Kayıt Sayfası</h1>
+            <button onClick={()=> setShowModal(true)} className='bg-gray-600 px-4 py-2 rounded-lg text-lg text-white'>Yeni Kayıt</button>
+            {showModal && <AddEmploye onClose={()=> setShowModal(false)}/>}
       <Employes />
     </div>
   )
